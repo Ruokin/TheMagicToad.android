9ce0f4b611e6b56df005f70f788a2c1716c631d8
Ruokin <m.doroganov@gmail.com>
2021-02-22 13:36:17 +0300

    Tower animations

--- Assets/Scripts/TowerScripts/PortalScript.cs
+++ Assets/Scripts/TowerScripts/PortalScript.cs
@@ -4,25 +4,20 @@ using UnityEngine.SceneManagement;
 public class PortalScript : MonoBehaviour
 {
     public GameObject PortalMenuUI;
-    public GameObject PortalParts;
-    
-    
+
+
     public void Portal()
     {
-        Instantiate(PortalParts);
         PortalMenuUI.SetActive(true);
-        Time.timeScale = 0f;
     }
     
     public void Swamp()
     {
         SceneManager.LoadScene("Zhaba");
-        Time.timeScale = 1f;
     }
     
     public void Close()
     {
         PortalMenuUI.SetActive(false);
-        Time.timeScale = 1f;
     }
 }
---
+++ Assets/Scripts/TowerScripts/SkyRotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class SkyRotation : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 1f) * Time.deltaTime);
+    }
+}
